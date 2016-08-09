9009973adb2b19df30243ed4f8b6e44a4829dd6d
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2016-08-09 17:25:30 +0200

    added simple tkinter client to test keyboard input

---
+++ test_client.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+import future
+from tkinter import *
+
+
+def print_event(event):
+    print('type={}, code={}, send_event={}, keysym={}'.format(event.type, event.keycode, event.send_event,
+                                                              event.keysym))
+
+if __name__ == '__main__':
+    root = Tk()
+
+    frame = Frame(root, width=200, height=100)
+    frame.bind("<KeyPress>", print_event)
+    frame.bind("<KeyRelease>", print_event)
+    frame.pack()
+    frame.focus_set()
+
+    root.mainloop()
