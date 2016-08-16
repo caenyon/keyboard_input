85e1e43d32b6f52ee1159aeea8c2d115d33e240f
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2016-08-16 02:27:42 +0200

    improved comments

--- keyboard_input/linux/send/send_uinput.py
+++ keyboard_input/linux/send/send_uinput.py
@@ -1,16 +1,29 @@
 # coding=utf-8
+"""
+This module contains a simple program that uses the uinput python package to create a new
+ user-space input device. This device can then be used to emit key events.
 
-# modified version of https://github.com/tuomasjjrasanen/python-uinput
+ Properties:
+ - requires uinput python package
+ - requires root!
 
-# requires root!
-
-import time
-
+ This script is a modified version of https://github.com/tuomasjjrasanen/python-uinput
+"""
 import uinput
+from time import sleep
+
 
 if __name__ == "__main__":
+
+    # define the keys of the virtual input device. In this example, we only create a device with
+    # a single key that sends an "x" character
     events = (uinput.KEY_X,)
 
+    # create virtual input device
     with uinput.Device(events) as device:
-        time.sleep(5)
+
+        # sleep 5 seconds
+        sleep(5)
+
+        # emit a single click (key press and key release event)
         device.emit_click(uinput.KEY_X)
